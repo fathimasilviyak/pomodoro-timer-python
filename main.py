0ed5f876d06b6a651e0bc63c8fdcf048ffeda6a2
fathimasilviyak <silviyakvm@gmail.com>
2023-04-06 20:48:31 +0530

    add more labels and buttons

--- main.py
+++ main.py
@@ -13,11 +13,25 @@ window = Tk()
 window.title("Pomodoro Timer")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-# display image
+# Add label
+timer_label = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+timer_label.grid(column=1, row=0)
+
+# Display image
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.pack()
+canvas.grid(column=1, row=1)
 # Add text on top of image
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+# Add start and reset buttons
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
+reset_button = Button(text="Restart", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✔", bg=YELLOW, fg=GREEN)
+check_mark.grid(column=1, row=3)
+
 window.mainloop()
