9f8557e660cba284b443bb3d3300420d040d2290
fathimasilviyak <silviyakvm@gmail.com>
2023-04-06 19:28:38 +0530

    setup UI, window and all contstants

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# UI setup
+window = Tk()
+window.title("Pomodoro Timer")
+
+
+window.mainloop()
